e51802f16c726d35c5da36ee6349a40554660972
EduardoRomaniniPessoa <eduardo.pessoa@etec.sp.gov.br>
2025-08-15 21:19:42 -0300

    teste de reprodução de tela

---
+++ teste/AppTimCss/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#f0f0ffff',
+        justifyContent: "space-around",
+        alignItems: "flex-start",
+      }}
+    >
+      <Text>Olá User</Text>
+    </View>
+  );
+}
